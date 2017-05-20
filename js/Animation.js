a89b4311afd4beaad6d01fe361bc0db16e8ba309
r3miz3r <r3miz3r@gmail.com>
2017-05-20 19:53:58 +0200

    Diverse pace of players

--- js/Animation.js
+++ js/Animation.js
@@ -26,8 +26,8 @@ function animationLoop(time){
     //player1
     pl01x+=dirPl01x;
     pl01y+=dirPl01y;
-    pl01diffX = (pl01x - x)*Player.speed;
-    pl01diffY = (pl01y - y)*Player.speed;
+    pl01diffX = (pl01x - x)*Player.speed*(Math.random()*(4/10)+0.80);
+    pl01diffY = (pl01y - y)*Player.speed*(Math.random()*(4/10)+0.80);
     distance01 = Math.sqrt((pl01x - x )*(pl01x - x )+(pl01y - y)*(pl01y - y));
     dirPl01x=(-1/distance01)*pl01diffX;
     dirPl01y=(-1/distance01)*pl01diffY;
@@ -35,8 +35,8 @@ function animationLoop(time){
     //player2
     pl02x+=dirPl02x;
     pl02y+=dirPl02y;
-    pl02diffX = (pl02x - x)*Player.speed;
-    pl02diffY = (pl02y - y)*Player.speed;
+    pl02diffX = (pl02x - x)*Player.speed*(Math.random()*(4/10)+0.80);
+    pl02diffY = (pl02y - y)*Player.speed*(Math.random()*(4/10)+0.80);
     distance02 = Math.sqrt((pl02x - x )*(pl02x - x )+(pl02y - y)*(pl02y - y));
     dirPl02x=(-1/distance02)*pl02diffX;
     dirPl02y=(-1/distance02)*pl02diffY;
